10f8a3c0ca07a0e3022080ad64d718b2b3da7a1e
Oriol Burgaya <oriolbv@gmail.com>
2020-07-31 18:29:07 +0200

    Player script changes

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private Rigidbody rb; 
+    public float dodgeSpeed = 5; 
+    public float rollSpeed = 5; 
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   // Use this for initialization 
+   void Start () 
+   { 
+        // Get access to our Rigidbody component 
+        rb = GetComponent<Rigidbody>(); 
+   } 
+
+   // Update is called once per frame 
+   void Update () 
+   { 
+        // Check if we're moving to the side 
+        var horizontalSpeed = Input.GetAxis("Horizontal") * dodgeSpeed; 
+        rb.AddForce(horizontalSpeed, 0, rollSpeed); 
+   }
 }
