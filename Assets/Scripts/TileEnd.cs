51ca46550bcbf255efc7a4dbb47f6cb1d754f758
Oriol Burgaya <oriolbv@gmail.com>
2020-08-01 15:53:13 +0200

    TileEnd behaviour script added

---
+++ Assets/Scripts/TileEnd.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileEnd : MonoBehaviour
+{
+    public float destroyTime = 1.5f; 
+
+    void OnTriggerEnter(Collider col) 
+    { 
+        // Check collision with Player
+        if (col.gameObject.GetComponent<Player>()) 
+        { 
+            // Spawn new tile
+            GameObject.FindObjectOfType<GameController>().SpawnNextTile(); 
+
+            // Destroy current tile after destroyTime
+            Destroy(transform.parent.gameObject, destroyTime); 
+        } 
+    } 
+}
