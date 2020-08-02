4a447bee3a727b8106a2edb79fcef63568acd35c
Oriol Burgaya <oriolbv@gmail.com>
2020-08-02 19:14:00 +0200

    Mobile touch inputs implemented

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -25,13 +25,67 @@ public class Player : MonoBehaviour
    { 
         // Get access to our Rigidbody component 
         rb = GetComponent<Rigidbody>(); 
-   } 
-
-   // Update is called once per frame 
-   void Update () 
-   { 
-        // Check if we're moving to the side 
-        var horizontalSpeed = Input.GetAxis("Horizontal") * dodgeSpeed; 
-        rb.AddForce(horizontalSpeed, 0, rollSpeed); 
    }
+
+    void Update()
+    {
+        // Movement in the x axis 
+        float horizontalSpeed = 0;
+
+        // Check if we are running either in the Unity editor or in a standalone build. 
+#if UNITY_STANDALONE || UNITY_WEBPLAYER
+
+    // Check if we're moving to the side 
+    horizontalSpeed = Input.GetAxis("Horizontal") * dodgeSpeed; 
+
+    // If the mouse is held down (or the screen is tapped on Mobile) 
+    if (Input.GetMouseButton(0)) 
+    { 
+        horizontalSpeed = CalculateMovement(Input.mousePosition); 
+    } 
+
+    //Check if we are running on a mobile device 
+#elif UNITY_IOS || UNITY_ANDROID
+
+        //Check if Input has registered more than zero touches 
+        if (Input.touchCount > 0)
+        {
+            //Store the first touch detected. 
+            Touch myTouch = Input.touches[0];
+            horizontalSpeed = CalculateMovement(myTouch.position);
+
+        }
+
+#endif
+
+        // Apply our auto-moving and movement forces 
+        rb.AddForce(horizontalSpeed, 0, rollSpeed);
+    }
+
+    /// <summary> 
+    /// Will figure out where to move the player horizontally 
+    /// </summary> 
+    /// <param name="pixelPos">The position the player has touched/clicked on</param> 
+    /// <returns>The direction to move in the x axis</returns> 
+    float CalculateMovement(Vector3 pixelPos)
+    {
+        // Converts to a 0 to 1 scale 
+        var worldPos = Camera.main.ScreenToViewportPoint(pixelPos);
+
+        float xMove = 0;
+
+        // If we press the right side of the screen 
+        if (worldPos.x < 0.5f)
+        {
+            xMove = -1;
+        }
+        else
+        {
+            // Otherwise we're on the left 
+            xMove = 1;
+        }
+
+        // Replace horizontalSpeed value
+        return xMove * dodgeSpeed;
+    }
 }
