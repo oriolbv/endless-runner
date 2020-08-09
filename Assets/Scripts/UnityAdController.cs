38c48a27b6822656f8670296267ca2dadf04c802
Oriol Burgaya <oriolbv@gmail.com>
2020-08-09 18:41:04 +0200

    Unity Ads script added

---
+++ Assets/Scripts/UnityAdController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+#if UNITY_ADS // Can only compile ad code on supported platforms 
+
+using UnityEngine.Advertisements; // Advertisement class
+
+#endif
+
+
+
+public class UnityAdController : MonoBehaviour
+{
+    public static void ShowAd()
+    {
+
+#if UNITY_ADS
+
+        if (Advertisement.IsReady())
+        {
+            Advertisement.Show();
+        }
+
+#endif
+
+    }
+}
